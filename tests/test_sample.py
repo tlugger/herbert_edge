fa4cb4b9074699b60d5bb3ef1adea4e348f4c85e
Tyler Lugger <tlugger@n.io>
2018-11-02 11:36:42 -0600

    Remove template files

--- tests/test_sample.py
+++
@@ -1,9 +0,0 @@
-from service_tests.service_test_case import NioServiceTestCase
-
-
-class TestSample(NioServiceTestCase):
-
-    service_name = 'PutServiceNameHere'
-
-    def test_function(self):
-        self.assertTrue(True)
